26e688adef67bd55e13665f7dcf4f8abd91bcfc9
kyj <elena.kim@devncore.org>
2021-07-22 16:17:38 +0900

    update

--- TicTacToeWPF/ViewModels/MainViewModel.cs
+++ TicTacToeWPF/ViewModels/MainViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Windows.Input;
 using TicTacToeWPF.Models;
 using TicTacToeWPF.Mvvm;
@@ -11,6 +10,7 @@ namespace TicTacToeWPF.ViewModels
     {
         private bool _playerTurn;
         private bool _isActivated;
+        private TicTacToeItem _winner;
         private List<TicTacToeItem> _source;
 
         public ICommand StartCommand { get; set; }
@@ -27,6 +27,12 @@ namespace TicTacToeWPF.ViewModels
         {
             get { return _isActivated; }
             set { _isActivated = value; OnPropertyChanged(); }
+        }
+
+        public TicTacToeItem Winner
+        {
+            get { return _winner; }
+            set { _winner = value; OnPropertyChanged(); }
         }
 
         public MainViewModel()
@@ -52,18 +58,58 @@ namespace TicTacToeWPF.ViewModels
         private void Start(object obj)
         {
             IsActivated = true;
+            Winner = null;
         }
 
         private void Restart(object obj)
         {
-            Source.ForEach(x => x.IsChecked = false);
-            IsActivated = false;
+            Source.ForEach(x => { x.IsChecked = false; x.PlayerType = null; });
+            IsActivated = true;
+            Winner = null;
         }
 
         private void Click(TicTacToeItem obj)
         {
             obj.PlayerType = _playerTurn ? "O" : "X";
             _playerTurn = !_playerTurn;
+
+            Winner = CalculateWinner(Source);
+
+            if (Winner != null)
+			{
+                IsActivated = false;
+            }
+        }
+
+        private TicTacToeItem CalculateWinner(List<TicTacToeItem> items)
+		{
+            List<int[]> arrayList = new List<int[]>()
+            {
+                new int[] { 0, 1, 2 },
+                new int[] { 3, 4, 5 },
+                new int[] { 6, 7, 8 },
+                new int[] { 0, 3, 6 },
+                new int[] { 1, 4, 7 },
+                new int[] { 2, 5, 8 },
+                new int[] { 0, 4, 8 },
+                new int[] { 2, 4, 6 }
+            };
+
+			for (int i = 0; i < arrayList.Count; i++)
+			{
+                var tempArray = arrayList[i];
+
+                var firstType = items[tempArray[0]].PlayerType;
+                var secondType = items[tempArray[1]].PlayerType;
+                var thirdType = items[tempArray[2]].PlayerType;
+
+                if (firstType == secondType && firstType == thirdType && firstType != null)
+				{
+                    return items[tempArray[0]];
+				}
+			}
+
+            return null;
         }
     }
 }
